0b55c60b4a827af39c516b166beb716a8d9fa0c5
Abhiru Karki <abhirukarki311@gmail.com>
2025-02-12 11:28:21 +0545

    Add Logic to Score Keeper

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
-let p1Var = 0;
-let p2Var = 0;
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
 
-const p1Score = document.querySelector("#p1-score");
-const p2Score = document.querySelector("#p2-score");
+const p1Span = document.querySelector("#p1-score");
+const p2Span = document.querySelector("#p2-score");
+const winningScoreSelect = document.querySelector("#playto");
 
 const p1Button = document.querySelector("#p1-button");
 const p2Button = document.querySelector("#p2-button");
 const reset = document.querySelector("#reset");
 
 p1Button.addEventListener("click", () => {
-  p1Var++;
-  p1Score.innerHTML = `${p1Var}`;
+  if (!isGameOver) {
+    p1Score++;
+    if (p1Score == winningScore) {
+      isGameOver = true;
+      p1Span.classList.add("winner");
+      p2Span.classList.add("loser");
+    }
+    p1Span.textContent = `${p1Score}`;
+  }
 });
 
 p2Button.addEventListener("click", () => {
-  p2Var++;
-  p2Score.innerHTML = `${p2Var}`;
+  if (!isGameOver) {
+    p2Score++;
+    if (p2Score == winningScore) {
+      isGameOver = true;
+      p2Span.classList.add("winner");
+      p1Span.classList.add("loser");
+    }
+    p2Span.textContent = `${p2Score}`;
+  }
 });
 
-reset.addEventListener("click", () => {
-  p1Var = 0;
-  p1Score.innerHTML = `${p1Var}`;
-  p2Var = 0;
-  p2Score.innerHTML = `${p2Var}`;
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  scoreReset();
 });
+
+reset.addEventListener("click", scoreReset);
+
+function scoreReset() {
+  p1Span.classList.remove("winner", "loser");
+  p2Span.classList.remove("winner", "loser");
+
+  isGameOver = false;
+
+  p1Score = 0;
+  p1Span.textContent = `${p1Score}`;
+
+  p2Score = 0;
+  p2Span.textContent = `${p2Score}`;
+}
